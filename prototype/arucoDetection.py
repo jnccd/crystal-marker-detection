8245eefe38dec6ecf8e5bb9e874409459aa68b0e
jnccd <kobidogao@outlook.com>
2023-05-12 16:26:02 +0200

    Add paper detection handling

--- prototype/arucoDetection.py
+++ prototype/arucoDetection.py
@@ -2,61 +2,41 @@ import numpy as np
 import time
 import cv2
 
-
-ARUCO_DICT = {
-	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
-}
-
-
 def aruco_display(corners, ids, rejected, image):
-	if len(corners) > 0:
+    
+    centers = []
+    verts = []
+    
+    if len(corners) > 0:
 		
-		ids = ids.flatten()
-		
-		for (markerCorner, markerID) in zip(corners, ids):
+        ids = ids.flatten()
+        
+        for (markerCorner, markerID) in zip(corners, ids):
 			
-			corners = markerCorner.reshape((4, 2))
-			(topLeft, topRight, bottomRight, bottomLeft) = corners
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
 			
-			topRight = (int(topRight[0]), int(topRight[1]))
-			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-			topLeft = (int(topLeft[0]), int(topLeft[1]))
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-			cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-			cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-			cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-			cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+            cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+            verts.append((topLeft, topRight, bottomRight, bottomLeft))
 			
-			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-			cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+            centers.append((cX, cY))
 			
-			cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-				0.5, (0, 255, 0), 2)
-			print("[Inference] ArUco marker ID: {}".format(markerID))
+            cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+            print("[Inference] ArUco marker ID: {}".format(markerID))
 			
-	return image
+    return image, centers, verts
 
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
 parameters =  cv2.aruco.DetectorParameters()
@@ -70,23 +50,38 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 while cap.isOpened():
     
-	ret, img = cap.read()
+    ret, img = cap.read()
 
-	h, w, _ = img.shape
+    h, w, _ = img.shape
 
-	width = 1000
-	height = int(width*(h/w))
-	img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+    width = 1000
+    height = int(width*(h/w))
+    img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
  
-	corners, ids, rejected = detector.detectMarkers(img)
+    corners, ids, rejected = detector.detectMarkers(img)
+    marked_img, centers, vertss = aruco_display(corners, ids, rejected, img)
+    
+    # Found paper
+    if len(corners) == 4:
+        ccx = int(np.mean([c[0] for c in centers]))
+        ccy = int(np.mean([c[1] for c in centers]))
+        print("Center", ccx, " ", ccy)
+        cv2.rectangle(marked_img, (ccx, ccy), (ccx+1, ccy+1), (0, 0, 255), 5)
+        
+        in_between_rect = []
+        for verts in vertss:
+            closest_to_center = min(verts, key=lambda v: abs(v[0] - ccx) + abs(v[1] - ccy))
+            in_between_rect.append(closest_to_center)
+            
+        for i in range(0,4):
+            j = (i+1) % 4
+            cv2.line(marked_img, in_between_rect[i], in_between_rect[j], (0, 0, 255), 2)
 
-	detected_markers = aruco_display(corners, ids, rejected, img)
+    cv2.imshow("Image", marked_img)
 
-	cv2.imshow("Image", detected_markers)
-
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-	    break
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
 
 cv2.destroyAllWindows()
 cap.release()
