80b38588ce8e52397adb1440e2fba0a0541ba3c1
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-05-22 17:41:54 +0200

    Add homography finding code and refactor

--- traindata-creator/createTraindata.py
+++ traindata-creator/createTraindata.py
@@ -3,6 +3,8 @@ import sys
 import cv2
 from pathlib import Path
 
+import numpy as np
+
 window_name = "Window"
 
 top_left_corner=[]
@@ -11,7 +13,7 @@ new_top_left = None
 cur_m_pos = None
 
 def mouseEvent(action, x, y, flags, *userdata):
-    global top_left_corner, bottom_right_corner, new_top_left, cur_m_pos
+    global window_name, top_left_corner, bottom_right_corner, new_top_left, cur_m_pos
   
     cur_m_pos = (x,y)
     if action == cv2.EVENT_LBUTTONDOWN:
@@ -78,8 +80,39 @@ def find_inner_rect(cornerss, ccx, ccy):
             in_between_rect[min_i] = (int(min_vert[0]), int(min_vert[1]))
             
         return in_between_rect
-
+    
+def get_opencv_aruco_detector():
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
+    parameters =  cv2.aruco.DetectorParameters()
+    return cv2.aruco.ArucoDetector(dictionary, parameters)
+    
+def find_homography_from_aruco(img, detector, width, height):
+    corners, ids, rejected = detector.detectMarkers(img)
+    marked_img, centers, cornerss = aruco_transform_and_display(corners, ids, rejected, img.copy())
+    
+    # Found paper
+    if len(corners) == 4:
+        # Find center
+        ccx = int(np.mean([c[0] for c in centers]))
+        ccy = int(np.mean([c[1] for c in centers]))
+        cv2.rectangle(marked_img, (ccx, ccy), (ccx+1, ccy+1), (0, 0, 255), 5)
+        
+        # Find inner rectangle
+        in_between_rect = find_inner_rect(cornerss)
+        for i in range(0,4):
+            j = (i+1)%4
+            cv2.line(marked_img, in_between_rect[i], in_between_rect[j], (0, 0, 255), 2)
+        
+        # Find Homography
+        src_rect  = np.array([[0, height, 1], [0, 0, 1], [width, 0, 1], [width, height, 1]])
+        dest_rect = np.array([[x,y,1] for (x,y) in in_between_rect])
+        h, status = cv2.findHomography(src_rect, dest_rect)
+        return h
+    
+    
 def main():
+    global window_name, top_left_corner, bottom_right_corner, new_top_left, cur_m_pos
+    
     # Prepare paths
     root_dir = Path(__file__).resolve().parent
     input_dir = root_dir / 'images'
@@ -91,14 +124,14 @@ def main():
         ]
     )
     
-    # Prepare aruco detector
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
-    parameters =  cv2.aruco.DetectorParameters()
-    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
-
-    # Load image/window and hook events
-    image = cv2.imread(input_img_paths[0])
-    height, width = image.shape[:2]
+    # Load first image and preprocess
+    img = cv2.imread(input_img_paths[0])
+    img_h, img_w = img.shape[:2]
+    detector = get_opencv_aruco_detector()
+    hg = find_homography_from_aruco(img, detector, 1, 1)
+    
+    
+    # Load window and hook events
     cv2.namedWindow(window_name)
     cv2.setMouseCallback(window_name, mouseEvent)
     
@@ -118,12 +151,12 @@ def main():
             print("Building...")
 
         # Draw
-        display_image = image.copy()
+        display_img = img.copy()
         for i in range(0, len(bottom_right_corner)):
-            cv2.rectangle(display_image, top_left_corner[i], bottom_right_corner[i], (0,255,0), 1, 8)
+            cv2.rectangle(display_img, top_left_corner[i], bottom_right_corner[i], (0,255,0), 1, 8)
         if new_top_left is not None:
-            cv2.rectangle(display_image, new_top_left, cur_m_pos, (0,255,0), 1, 8)
-        cv2.imshow(window_name, display_image)
+            cv2.rectangle(display_img, new_top_left, cur_m_pos, (0,255,0), 1, 8)
+        cv2.imshow(window_name, display_img)
     
     cv2.destroyAllWindows()
 
