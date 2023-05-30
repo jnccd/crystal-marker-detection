b6f9e91dc334d1467184b2c4dd8c7cc3815df68c
jnccd <kobidogao@outlook.com>
2023-05-31 01:16:27 +0200

    Work on tf obj detect module

--- object-detection-tf/object_detection_tf/runconfigs.py
+++ object-detection-tf/object_detection_tf/runconfigs.py
@@ -4,28 +4,38 @@ from tensorflow import optimizers as optis
 from keras import losses as loss
 from keras.callbacks import LearningRateScheduler
 
-from object_detection_tf.loss import dice_coef_loss, dice_coef_2_loss, bcross_mod_fun_loss, BinaryCrossEntropyLoss, BinaryCrossEntropyLossFlat
+from object_detection_tf.loss import RetinaNetLoss
+from object_detection_tf.model import RetinaNet, get_resnet_backbone
 
-lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+exp_decay_learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate=1e-3,
     decay_steps=100,
     decay_rate=0.9)
+
+constant_decay_learning_rates = [2.5e-06, 0.000625, 0.00125, 0.0025, 0.00025, 2.5e-05]
+constant_decay_learning_rate_boundaries = [125, 250, 500, 240000, 360000]
+constant_decay_learning_rate = tf.optimizers.schedules.PiecewiseConstantDecay(
+    boundaries=constant_decay_learning_rate_boundaries, values=constant_decay_learning_rates
+)
 
 @dataclass
 class RunConfig:
     name: str
     loss: any
-    optimizer: any
-    callbacks: list[tf.keras.callbacks.Callback]
+    optimizer: any = "adam"
+    model: any = None
+    callbacks: list[tf.keras.callbacks.Callback] = []
     normalize_input_data: bool = False
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
 
 configs = [
-    RunConfig( "dice",              dice_coef_loss,                             "adam", []),
-    RunConfig( "dice-cadam",        dice_coef_loss,                             "adam", [LearningRateScheduler(lr_schedule, verbose=1)]),
+    RunConfig( name="retinaNet", 
+              loss=RetinaNetLoss(num_classes=1), 
+              model=RetinaNet(1, get_resnet_backbone()), 
+              callbacks=[constant_decay_learning_rate] ),
     
-    RunConfig( "bcross",            "binary_crossentropy",                      "adam", []),
+    RunConfig( name="bcross", loss="binary_crossentropy"),
     ]
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
