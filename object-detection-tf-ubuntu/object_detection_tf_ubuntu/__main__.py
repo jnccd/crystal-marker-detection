3c12866a8e6044a9e6f97d1362d8dd29c44ef4ef
jnccd <kobidogao@outlook.com>
2023-06-05 18:57:43 +0200

    Init new ubuntu tf module

---
+++ object-detection-tf-ubuntu/object_detection_tf_ubuntu/__main__.py
@@ -0,0 +1,5 @@
+import object_detection_tf_ubuntu
+import sys
+
+if __name__ == '__main__':
+    object_detection_tf_ubuntu.main()
---
+++ object-detection-tf-ubuntu/object_detection_tf_ubuntu/datasets.py
@@ -0,0 +1,7 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.utils import load_img
+
+def create_datasets():
+    # TODO
+    return None
