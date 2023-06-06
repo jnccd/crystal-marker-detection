acc4512f2a6acc5b043013c0de4ff352244954ae
jnccd <kobidogao@outlook.com>
2023-06-06 17:46:38 +0200

    Try arranging markers like in img

--- prototype-cv/testSift.py
+++ prototype-cv/testSift.py
@@ -4,13 +4,15 @@ from pathlib import Path
 
 root_dir = Path(__file__).resolve().parent
 border_marker_img_path = root_dir/'sift-base'/'border-marker-scaled.png'
-in_img_marker_img_path = root_dir/'sift-base'/'in-img-marker-scaled.png'
+in_img_marker_img_path = root_dir/'sift-base'/'in-img-marker-scaled-arranged.png'
 test_img_path = Path("N:\Downloads\Archives\FabioBilder\\the_good_pics_for_sift\DSC_3741.JPG")
 
 img1 = cv2.imread(str(in_img_marker_img_path))
 img2 = cv2.imread(str(test_img_path))
 
-img2 = cv2.resize(img2, dsize=(600, 400), interpolation=cv2.INTER_CUBIC)
+size_mult = 2
+
+img2 = cv2.resize(img2, dsize=(600*size_mult, 400*size_mult), interpolation=cv2.INTER_CUBIC)
 
 cv2.imshow('in img',img2)
 
@@ -20,9 +22,9 @@ foreground_col = np.array([150, 139, 127])
 imgg = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 #imgr = cv2.inRange(img2, foreground_col, background_col)
 imgg = cv2.adaptiveThreshold(imgg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,31,2)
+            cv2.THRESH_BINARY,50*size_mult+1,2)
 imgg = 255-imgg
-imgg = cv2.GaussianBlur(imgg,(3,3),0)
+#imgg = cv2.GaussianBlur(imgg,(3,3),0)
 final_img = imgg
 
 cv2.imshow('Thresholded img',final_img)
@@ -39,7 +41,9 @@ kp2, des2 = sift.detectAndCompute(final_img,None)
 matches = bf.match(desl,des2)
 matches = sorted(matches, key = lambda x:x.distance)
 
-img3 = cv2.drawMatches(img1, kpl, final_img, kp2, matches, final_img, flags=2)
+print(len(matches))
+
+img3 = cv2.drawMatches(img1, kpl, final_img, kp2, matches[:20], final_img, flags=2)
 
 cv2.imshow('SIFT',img3)
 cv2.waitKey(0)
