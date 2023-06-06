65be18d9f0ae92570d9141959cf96c2df8b77db6
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-06-06 17:51:50 +0200

    Show only the best matches

--- prototype-cv/testSift.py
+++ prototype-cv/testSift.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 root_dir = Path(__file__).resolve().parent
 border_marker_img_path = root_dir/'sift-base'/'border-marker-scaled.png'
-in_img_marker_img_path = root_dir/'sift-base'/'in-img-marker-scaled-arranged.png'
+in_img_marker_img_path = root_dir/'sift-base'/'in-img-marker-scaled.png'
 test_img_path = Path("N:\Downloads\Archives\FabioBilder\\the_good_pics_for_sift\DSC_3741.JPG")
 
 img1 = cv2.imread(str(in_img_marker_img_path))
@@ -16,8 +16,8 @@ img2 = cv2.resize(img2, dsize=(600*size_mult, 400*size_mult), interpolation=cv2.
 
 cv2.imshow('in img',img2)
 
-background_col = np.array([151, 164, 186])
-foreground_col = np.array([150, 139, 127])
+# background_col = np.array([151, 164, 186])
+# foreground_col = np.array([150, 139, 127])
 
 imgg = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 #imgr = cv2.inRange(img2, foreground_col, background_col)
@@ -41,9 +41,10 @@ kp2, des2 = sift.detectAndCompute(final_img,None)
 matches = bf.match(desl,des2)
 matches = sorted(matches, key = lambda x:x.distance)
 
-print(len(matches))
+n = len(matches)
+print(n)
 
-img3 = cv2.drawMatches(img1, kpl, final_img, kp2, matches[:20], final_img, flags=2)
+img3 = cv2.drawMatches(img1, kpl, final_img, kp2, matches[:int(n/2)], final_img, flags=2)
 
 cv2.imshow('SIFT',img3)
 cv2.waitKey(0)
