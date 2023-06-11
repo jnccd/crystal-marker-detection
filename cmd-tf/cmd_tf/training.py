97363118a254d0eb0c65411484dfd39daa326f76
jnccd <kobidogao@outlook.com>
2023-06-11 04:10:01 +0200

    Update for new traindata layout

--- cmd-tf/cmd_tf/training.py
+++ cmd-tf/cmd_tf/training.py
@@ -14,17 +14,18 @@ from tensorflow.keras.utils import load_img, array_to_img
 
 from cmd_tf.runconfigs import RunConfig, configs, load_runconfig
 from cmd_tf.dataloader import XUnetBatchgen
+from cmd_tf.utility import get_files_from_folders_with_ending
 
 num_classes = 1
 
 def fit(
-    batch_size: int = 32, 
+    batch_size: int = 16, 
     num_epochs: int = 1, 
     run: str = 'default', 
     data_folder: str = 'renders', 
-    size: int = 160,
-    print_model: bool = False,
-    use_multi_gpu_strategy: bool = False,
+    size: int = 160, 
+    print_model: bool = False, 
+    use_multi_gpu_strategy: bool = False, 
     ):
 
     img_size = (size, size)
@@ -32,42 +33,37 @@ def fit(
     # --- Paths ---
     # Base
     root_dir = Path(__file__).resolve().parent
-    render_dir = root_dir / data_folder
-    # Server Base Paths
-    if not os.path.isdir(render_dir):
+    dataset_dir = data_folder
+    # MIP Server Base Paths
+    if not os.path.isdir(dataset_dir):
         root_dir = Path("/data/cmdtf")
-        render_dir = root_dir / data_folder
-    # Training Data Paths
-    input_dir = render_dir / 'images'
-    target_dir = render_dir / 'segmentations'
+        dataset_dir = root_dir / data_folder
     # Set Run Paths
     runs_dir = root_dir / 'runs'
     run_dir = runs_dir / f'run-{run}'
     val_dir = run_dir / 'validation'
     weights_dir = run_dir / 'weights'
     # Prepare Training Data Img Paths
-    num_samples = os.listdir(input_dir).__len__()
-    input_img_paths = sorted(
-        [
-            os.path.join(input_dir, fname)
-            for fname in os.listdir(input_dir)
-            if fname.endswith(".png")
-        ]
-    )
-    target_img_paths = sorted(
-        [
-            os.path.join(target_dir, fname)
-            for fname in os.listdir(target_dir)
-            if fname.endswith(".png")
-        ]
-    )
+    train_dir = dataset_dir / 'train'
+    val_dir = dataset_dir / 'val'
+    train_x_paths = get_files_from_folders_with_ending([train_dir], "_in.png")
+    train_y_paths = get_files_from_folders_with_ending([train_dir], "_seg.png")
+    val_x_paths = get_files_from_folders_with_ending([val_dir], "_in.png")
+    val_y_paths = get_files_from_folders_with_ending([val_dir], "_seg.png")
+    random.Random(1337).shuffle(train_x_paths)
+    random.Random(1337).shuffle(train_y_paths)
+    random.Random(420).shuffle(val_x_paths)
+    random.Random(420).shuffle(val_y_paths)
+    
+    num_train_samples = len(train_x_paths)
+    num_val_samples = len(val_x_paths)
     
     # Load runconfig
     cur_conf = load_runconfig(run)
     
     print()
     print("---Configuration---------------------------------")
-    print("Number of samples:", num_samples)
+    print("Number of training samples:", num_train_samples)
     print("Batch Size:", batch_size)
     print("Epochs:", num_epochs)
     print("Run Name:", run)
@@ -91,42 +87,27 @@ def fit(
         if print_model:
             model.summary()
             tf.keras.utils.plot_model(model, to_file=run_dir / "model.png", show_shapes=True)
-            
-
-        # Split our img paths into a training and a validation set
-        print("Create img sets...")
-        train_input_img_paths = [ img for img in input_img_paths if not startswith_any(os.path.basename(img), val_prefixes) ]
-        train_target_img_paths = [ img for img in target_img_paths if not startswith_any(os.path.basename(img), val_prefixes) ]
-        val_input_img_paths = [ img for img in input_img_paths if startswith_any(os.path.basename(img), val_prefixes) ]
-        val_target_img_paths = [ img for img in target_img_paths if startswith_any(os.path.basename(img), val_prefixes) ]
-        
-        random.Random(1337).shuffle(train_input_img_paths)
-        random.Random(1337).shuffle(train_target_img_paths)
-        random.Random(420).shuffle(val_input_img_paths)
-        random.Random(420).shuffle(val_target_img_paths)
         
         print()
         print("---Train/Val Data Validation---------------------------------")
         
-        print("Train in imgs:", train_input_img_paths.__len__(), "| Train target imgs:", train_target_img_paths.__len__())
-        for input_path, target_path in zip(train_input_img_paths[:3], train_target_img_paths[:3]):
+        print("Train in imgs:", train_x_paths.__len__(), "| Train target imgs:", train_y_paths.__len__())
+        for input_path, target_path in zip(train_x_paths[:3], train_y_paths[:3]):
             print(os.path.basename(input_path), "|", os.path.basename(target_path))
         
-        print("Val in imgs:", val_input_img_paths.__len__(), "| Val target imgs:", val_target_img_paths.__len__())
-        for input_path, target_path in zip(val_input_img_paths[:3], val_target_img_paths[:3]):
+        print("Val in imgs:", val_x_paths.__len__(), "| Val target imgs:", val_y_paths.__len__())
+        for input_path, target_path in zip(val_x_paths[:3], val_y_paths[:3]):
             print(os.path.basename(input_path), "|", os.path.basename(target_path))
         
-        val_samples = val_input_img_paths.__len__()
-        epoch_steps = math.floor((num_samples - val_samples) / batch_size)
+        epoch_steps = math.floor((num_train_samples - num_val_samples) / batch_size)
         print("Steps per Epoch:", epoch_steps)
         
         print("-------------------------------------------------------------")
         print()
 
         # Instantiate data Sequences for each split
-        train_gen = ResnetBatchgen(batch_size, img_size, train_input_img_paths, train_target_img_paths, cur_conf)
-        val_gen = ResnetBatchgen(batch_size, img_size, val_input_img_paths, val_target_img_paths, cur_conf)
-
+        train_gen = XUnetBatchgen(batch_size, img_size, train_x_paths, train_y_paths)
+        val_gen = XUnetBatchgen(batch_size, img_size, val_x_paths, val_y_paths)
 
         print("Compile model...")
         metrics = [tf.keras.metrics.BinaryAccuracy(),
@@ -157,7 +138,7 @@ def fit(
 
         print("Write evaluation...")
         # First eval textfile
-        val_gen = ResnetBatchgen(batch_size, img_size, val_input_img_paths, val_target_img_paths, cur_conf)
+        val_gen = XUnetBatchgen(batch_size, img_size, val_x_paths, val_y_paths)
         eval_results = model.evaluate(val_gen)
         eval_file = run_dir / 'evals'
         i = 0
@@ -198,20 +179,20 @@ def fit(
         
         print("Write validation...")
         # Generate predictions for all images in the validation set
-        val_gen = ResnetBatchgen(batch_size, img_size, val_input_img_paths, val_target_img_paths, cur_conf)
+        val_gen = XUnetBatchgen(batch_size, img_size, val_x_paths, val_y_paths)
         val_preds = model.predict(val_gen)
         
         if not os.path.exists(val_dir):
             os.makedirs(val_dir)
 
         # Display some results for validation images
-        for i in range(0, min(50, val_samples)):
+        for i in range(0, min(50, num_val_samples)):
             # Display input image
-            inimg = ImageOps.autocontrast(load_img(val_input_img_paths[i]))
+            inimg = ImageOps.autocontrast(load_img(val_x_paths[i]))
             inimg.save(val_dir / f'{i}_input.png')
 
             # Display ground-truth target mask
-            img = ImageOps.autocontrast(load_img(val_target_img_paths[i]))
+            img = ImageOps.autocontrast(load_img(val_y_paths[i]))
             img.save(val_dir / f'{i}_target_output.png')
 
             # Display mask predicted by our model
