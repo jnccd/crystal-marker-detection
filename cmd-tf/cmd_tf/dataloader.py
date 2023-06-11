f0d6476bf88dbd5326629d3e75f0b59e68749c04
jnccd <kobidogao@outlook.com>
2023-06-11 04:09:45 +0200

    Remove conf in batchget args

--- cmd-tf/cmd_tf/dataloader.py
+++ cmd-tf/cmd_tf/dataloader.py
@@ -5,12 +5,11 @@ from tensorflow.keras.utils import load_img
 class XUnetBatchgen(keras.utils.Sequence):
     """Helper to iterate over the data (as Numpy arrays)."""
 
-    def __init__(self, batch_size, img_size, input_img_paths, target_img_paths, cur_conf):
+    def __init__(self, batch_size, img_size, input_img_paths, target_img_paths):
         self.batch_size = batch_size
         self.img_size = img_size
         self.input_img_paths = input_img_paths
         self.target_img_paths = target_img_paths
-        self.cur_conf = cur_conf
 
     def __len__(self):
         return len(self.target_img_paths) // self.batch_size
