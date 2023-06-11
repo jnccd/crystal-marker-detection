dfdeebd08c788c047d888a0c134752e669831f7e
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-06-11 04:10:01 +0200

    Update for new traindata layout

--- cmd-tf/cmd_tf/utility.py
+++ cmd-tf/cmd_tf/utility.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+import shutil
 from typing import List
 
 import tensorflow as tf
@@ -5,7 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # String stuff
-
 def startswith_any(filename: str, prefs: List[str]):
     for pref in prefs:
         if filename.startswith(pref):
@@ -13,4 +15,20 @@ def startswith_any(filename: str, prefs: List[str]):
     return False
 
 # Implementing utility functions
-
+def get_files_from_folders_with_ending(folders, ending):
+    paths = []
+    for folder in folders:
+        paths.extend(sorted(
+            [
+                os.path.join(folder, fname)
+                for fname in os.listdir(folder)
+                if fname.endswith(ending)
+            ]
+        ))
+    return paths
+def create_dir_if_not_exists(dir: Path, clear = False):
+    if clear and os.path.isdir(dir):
+        shutil.rmtree(dir)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    return dir
