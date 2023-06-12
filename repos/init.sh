551dab24ee1c628324c18832e9f2831450f64aac
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-06-13 01:31:03 +0200

    Update repos init script

--- repos/init.sh
+++ repos/init.sh
@@ -2,11 +2,16 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) 
 echo 'Hi, Im in '$SCRIPT_DIR
-
 cd $SCRIPT_DIR
 
+# Yolov5
 git clone https://github.com/ultralytics/yolov5
 cd yolov5
 pip install -r requirements.txt
+cd ..
 
+# Tensorflow Models
+git clone https://github.com/tensorflow/models.git
+cd models
+# TODO: Installing or something
 cd ..
