a14ca9d5e0b0e3db2e5fb3b84f1d4497a3e34623
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-06-17 18:52:38 +0200

    Trim init script

--- repos/init.sh
+++ repos/init.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) 
-echo 'Hi, Im in '$SCRIPT_DIR
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 # Yolov5
@@ -10,8 +9,8 @@ cd yolov5
 #pip install -r requirements.txt
 cd ..
 
-# Tensorflow Models
-git clone https://github.com/tensorflow/models.git
-cd models
-# TODO: Installing or something
-cd ..
+# # Tensorflow Models
+# git clone https://github.com/tensorflow/models.git
+# cd models
+# # TODO: Installing
+# cd ..
