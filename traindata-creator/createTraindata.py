f3c31205473592c43d5b357aac47aa1ebeda8f13
jnccd <kobidogao@outlook.com>
2023-06-18 14:35:19 +0200

    Add todo

--- traindata-creator/createTraindata.py
+++ traindata-creator/createTraindata.py
@@ -286,6 +286,7 @@ def build_traindata(input_img_paths, detector, img_w, img_h, marked_dir, train_d
             circs = mult_by_point(circs, (new_size[1] / inner_bounds_w, new_size[0] / inner_bounds_h))
             circs = add_by_point(circs, (left, top))
             out_img_size = (resize_size, resize_size)
+        # TODO: Add augmentation code here?
         gcircs = unflatten(circs, 4) 
         bgcircs = [get_bounds(x) for x in gcircs] # boundsOf-grouped-cropped-inner-rect-corners
         # Normalize coordinates
