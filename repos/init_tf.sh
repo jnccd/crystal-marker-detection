17b5cc4b839fbb52797542a566192d1dfeb205d9
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-06-19 19:31:31 +0200

    Split init files for tf and pt envs

--- repos/init_tf.sh
+++ repos/init_tf.sh
@@ -3,10 +3,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-# Yolov5
-git clone https://github.com/ultralytics/yolov5
-cd yolov5
-#pip install -r requirements.txt
+# Retinanet Model
+git clone https://github.com/fizyr/keras-retinanet
+cd keras-retinanet
+pip install . --user
 cd ..
 
 # # Tensorflow Models
