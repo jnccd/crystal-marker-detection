bac17cf64d51c22e194868ff2c4be8d8ebbea270
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-07-03 18:50:03 +0200

    Add simple argument support to yolo train loop sh

--- yolo_train_loop.sh
+++ yolo_train_loop.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
-run_name=test_sh_w_hyp
+run_name=test_whyp
 dataset_name=yolov5-640-aug-good-pics-v2
 valset_name=yolov5-640-on-skin-valset
 
-python repos/yolov5/train.py --name $run_name --img 640 --batch 8 --epochs 300 --data traindata-creator/dataset/$dataset_name/$dataset_name.yaml --weights yolov5s.pt #--hyp hyp.no-augmentation.yaml 
+# Apply start args
+if [ "$1" != '' ]; then
+    run_name=$1
+	echo "Set runname to $run_name!"
+fi
+if [ "$2" != '' ]; then
+    dataset_name=$2
+	echo "Set dataset_name to $dataset_name!"
+fi
+
+# Set parameter args
+if [[ $run_name == *whyp ]]; then
+    hyp=''
+else
+    hyp='--hyp hyp.no-augmentation.yaml'
+fi
+echo "hyp is '$hyp'!"
+
+python repos/yolov5/train.py --name $run_name --img 640 --batch 8 --epochs 300 --data traindata-creator/dataset/$dataset_name/$dataset_name.yaml --weights yolov5s.pt $hyp
 rm ./yolov5s.pt
 python repos/yolo_gen_evaldata.py -r $run_name -df traindata-creator/dataset/$valset_name/
 python -m cmd_tf -av repos/evaldata/yolov5-$run_name/
+cat repos/evaldata/yolov5-$run_name/evals/evals.json
