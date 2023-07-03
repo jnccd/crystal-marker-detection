3550fb410f4cd6477cf59d351592de4aa78a91b6
jnccd <kobidogao@outlook.com>
2023-07-03 19:19:58 +0200

    Unify init files

--- repos/init.sh
+++ repos/init.sh
@@ -3,14 +3,14 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
+# Yolov5
+git clone https://github.com/ultralytics/yolov5
+cd yolov5
+pip install -r requirements.txt
+cd ..
+
 # Retinanet Model
 git clone https://github.com/fizyr/keras-retinanet
 cd keras-retinanet
 pip install . --user
 cd ..
-
-# # Tensorflow Models
-# git clone https://github.com/tensorflow/models.git
-# cd models
-# # TODO: Installing
-# cd ..
