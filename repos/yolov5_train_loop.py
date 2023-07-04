b2c46bfe6c4b95ef5114633a6a9a4437ef900623
jnccd <kobidogao@outlook.com>
2023-07-04 11:29:23 +0200

    Implement other args

--- repos/yolov5_train_loop.py
+++ repos/yolov5_train_loop.py
@@ -11,10 +11,10 @@ parser.add_argument('-n','--run-name', type=str, default='default', help='Define
 parser.add_argument('-d','--dataset-name', type=str, default='yolov5-640-aug-good-pics-v2', help='Defines the dataset to train from.')
 parser.add_argument('-v','--valset-name', type=str, default='yolov5-640-on-skin-valset', help='Defines the valset to test against.')
 parser.add_argument('-na','--no-aug', action='store_true', help='Disables the yolov5 repo augmentations.')
-parser.add_argument('-s','--img-size', type=int, default=640, help='.')
-parser.add_argument('-b','--batch-size', type=int, default=8, help='.')
-parser.add_argument('-e','--epochs', type=int, default=100, help='.')
-parser.add_argument('-m','--model', type=str, default='yolov5s', help='.')
+parser.add_argument('-s','--img-size', type=int, default=640, help='Sets the img size of the model.')
+parser.add_argument('-b','--batch-size', type=int, default=8, help='Sets the batch size to train with.')
+parser.add_argument('-e','--epochs', type=int, default=100, help='Sets the epochs to train for.')
+parser.add_argument('-m','--model', type=str, default='yolov5s', help='Sets the model to train with.')
 args = parser.parse_args()
 
 project_folder = Path('repos/training/yolov5')
@@ -23,10 +23,11 @@ yolov5_args = ''
 if args.no_aug:
     yolov5_args += '--hyp hyp.no-augmentation.yaml'
 
-print('Training...')
-os.system(f'python repos/yolov5/train.py --name {args.run_name} --img 640 --batch 8 --epochs 3 --project {project_folder} --data traindata-creator/dataset/{args.dataset_name}/{args.dataset_name}.yaml --weights {args.model}.pt {yolov5_args}')
+print('--- Training...')
+os.system(f'python repos/yolov5/train.py --name {args.run_name} --img {args.img_size} --batch {args.batch_size} --epochs {args.epochs} --project {project_folder} --data traindata-creator/dataset/{args.dataset_name}/{args.dataset_name}.yaml --weights {args.model}.pt {yolov5_args}')
 os.system(f'rm {args.model}.pt')
-print('Evaluating...')
+print('--- Evaluating...')
 os.system(f'python repos/yolov5_gen_evaldata.py -r {args.run_name} -df traindata-creator/dataset/{args.valset_name}/')
 os.system(f'python -m cmd_tf -av repos/evaldata/yolov5/{args.run_name}/')
+os.system(f'cp traindata-creator/dataset/{args.dataset_name}/')
 #os.system(f'cat repos/evaldata/yolov5/{args.run_name}/evals/evals.json')
