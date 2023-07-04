a7ddc1ceea570482d9ef393280381c6e8bbe1b77
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-07-04 12:42:31 +0200

    Update batch train for automated yolo train of all yolo datasets

--- batch_train.sh
+++ batch_train.sh
@@ -1,3 +1,5 @@
+SECONDS=0
+
 # Just train everything a bit
 #python -m cmd_tf -df traindata-creator/dataset/seg-red-rects/ -r sm-unet-red-rects -bs 8 -e 75
 #python -m cmd_tf -df traindata-creator/dataset/seg-good-pics-ratio-val/ -r sm-unet-aruco -bs 8 -e 100
@@ -6,5 +8,27 @@
 #python -m cmd_tf -df traindata-creator/dataset/seg-good-pics-ratio-val/ -r sm-psnet-aruco -bs 4 -e 100
 
 # Test if output stays somewhat the same 
-python -m cmd_tf -df traindata-creator/dataset/seg-good-pics-ratio-val/ -r sm-unet-aruco-same1 -bs 8 -e 5
-python -m cmd_tf -df traindata-creator/dataset/seg-good-pics-ratio-val/ -r sm-unet-aruco-same2 -bs 8 -e 5
+# python -m cmd_tf -df traindata-creator/dataset/seg-good-pics-ratio-val/ -r sm-unet-aruco-same1 -bs 8 -e 5
+# python -m cmd_tf -df traindata-creator/dataset/seg-good-pics-ratio-val/ -r sm-unet-aruco-same2 -bs 8 -e 5
+
+for dir in traindata-creator/dataset/yolov5-*/; do
+ dirname=$(basename "$dir")
+
+ if [[ $dirname == *"-valset" ]]; then
+  echo "I wont test $dirname"
+  continue
+ fi
+ 
+ cat traindata-creator/dataset/$dirname/dataset-def.json 
+
+ python repos/yolov5_train_loop.py -n test-$dirname-1 -d $dirname -e 300 --no-aug
+ #python repos/yolov5_train_loop.py -n test-$dirname-2 -d $dirname -e 300 --no-aug
+ #python repos/yolov5_train_loop.py -n test-$dirname-3 -d $dirname -e 300 --no-aug
+ python repos/yolov5_train_loop.py -n test-$dirname-yolo5aug-1 -d $dirname -e 300 --no-aug
+ #python repos/yolov5_train_loop.py -n test-$dirname-yolo5aug-2 -d $dirname -e 300 --no-aug
+ #python repos/yolov5_train_loop.py -n test-$dirname-yolo5aug-3 -d $dirname -e 300 --no-aug
+ 
+done
+
+duration=$SECONDS
+echo "Batch training took $(($duration / 60)) minutes and $(($duration % 60)) seconds."
