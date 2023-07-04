983808264d4e2a5bf614a12414b86c129b2d004f
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-07-04 18:39:40 +0200

    Add overlay_transparent()

--- traindata-creator/utils.py
+++ traindata-creator/utils.py
@@ -180,6 +180,46 @@ def create_random_persp_mat(img_size_wh, perspective_strength = 0.3):
         print('Invalid side!')
     return cv2.getPerspectiveTransform(src_points, dst_points)
 
+# Taken from https://gist.github.com/clungzta/b4bbb3e2aa0490b0cfcbc042184b0b4e
+def overlay_transparent(background_img, img_to_overlay_t, x, y, overlay_size=None):
+	"""
+	@brief      Overlays a transparant PNG onto another image using CV2
+	
+	@param      background_img    The background image
+	@param      img_to_overlay_t  The transparent image to overlay (has alpha channel)
+	@param      x                 x location to place the top-left corner of our overlay
+	@param      y                 y location to place the top-left corner of our overlay
+	@param      overlay_size      The size to scale our overlay to (tuple), no scaling if None
+	
+	@return     Background image with overlay on top
+	"""
+	
+	bg_img = background_img.copy()
+	
+	if overlay_size is not None:
+		img_to_overlay_t = cv2.resize(img_to_overlay_t.copy(), overlay_size)
+
+	# Extract the alpha mask of the RGBA image, convert to RGB 
+	b,g,r,a = cv2.split(img_to_overlay_t)
+	overlay_color = cv2.merge((b,g,r))
+	
+	# Apply some simple filtering to remove edge noise
+	mask = cv2.medianBlur(a,5)
+
+	h, w, _ = overlay_color.shape
+	roi = bg_img[y:y+h, x:x+w]
+
+	# Black-out the area behind the logo in our original ROI
+	img1_bg = cv2.bitwise_and(roi.copy(),roi.copy(),mask = cv2.bitwise_not(mask))
+	
+	# Mask out the logo from the logo image.
+	img2_fg = cv2.bitwise_and(overlay_color,overlay_color,mask = mask)
+
+	# Update the original image with our new ROI
+	bg_img[y:y+h, x:x+w] = cv2.add(img1_bg, img2_fg)
+
+	return bg_img
+
 # --- Traindata imgs -------------------------------------------------------------------------------------------------------------------------
 
 def resize_and_pad_with_labels(img: Mat, desired_size: int, polys: list[Polygon], background_color = [0, 0, 0], border_type = cv2.BORDER_CONSTANT):
