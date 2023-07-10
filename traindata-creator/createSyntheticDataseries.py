8a96d20aa605bbec1019aa4a6b64070907a9130d
jnccd <kobidogao@outlook.com>
2023-07-10 23:24:34 +0200

    Add strip to synth images

--- traindata-creator/createSyntheticDataseries.py
+++ traindata-creator/createSyntheticDataseries.py
@@ -16,7 +16,7 @@ background_textures_dir = assets_dir / 'background_textures'
 
 aruco_img = cv2.imread(str(aruco_texture_path))
 aruco_img = cv2.cvtColor(aruco_img, cv2.COLOR_RGB2RGBA)
-aruco_img_size = int(target_size/10)
+aruco_img_size = int(target_size/15)
 aruco_img = cv2.resize(aruco_img, (aruco_img_size, aruco_img_size), interpolation=cv2.INTER_NEAREST)
 
 back_textures = [cv2.imread(str(p)) for p in get_files_from_folders_with_ending([background_textures_dir][:3], '.jpg')]
@@ -28,21 +28,34 @@ for i in range(num_pics_to_gen):
     back_img = center_crop_to_11_aspect_ratio(back_textures[i % len(back_textures)], target_size)
     fore_img = center_crop_to_11_aspect_ratio(fore_textures[i % len(fore_textures)], target_size)
     
+    aruco_marker_x = int(fore_img.shape[1]/2 - aruco_img.shape[1]/2) + random.randint(-int(target_size/10), int(target_size/10))
+    aruco_marker_y = int(fore_img.shape[0]/2 - aruco_img.shape[0]/2) + random.randint(-int(target_size/10), int(target_size/10))
+    
+    strip_img = np.zeros((target_size, target_size) + (3,), dtype = np.uint8)
+    strip_img = cv2.cvtColor(strip_img, cv2.COLOR_RGB2RGBA)
+    strip_brightness = random.randrange(50, 100)
+    strip_width = int(target_size/5)
+    strip_opacity = 100
+    strip_img[:, :, 3] = 0
+    cv2.rectangle(strip_img, (0, int(target_size/2 - strip_width/2)), (target_size, int(target_size/2 + strip_width/2)), (strip_brightness, strip_brightness, strip_brightness, strip_opacity), -1)
+    strip_img[aruco_marker_y:aruco_marker_y+aruco_img.shape[0], aruco_marker_x:aruco_marker_x+aruco_img.shape[1], 3] = aruco_img[:, :, 0].astype('float32') / 255 * 100
+    cv2.imwrite(str(root_dir / 'strip_img.png'), strip_img)
+    
     # Add aruco img to fore img and add alpha channel
     #fore_img = overlay_transparent(fore_img, aruco_img, int(fore_img.shape[1]/2 - aruco_img.shape[1]/2), int(fore_img.shape[0]/2 - aruco_img.shape[0]/2))
     #fore_img = cv2.addWeighted(fore_img,0.4,aruco_img,0.1,0)
+    # fore_img = combine_two_color_images(
+    #     fore_img, 
+    #     aruco_img, 
+    #     aruco_marker_x,
+    #     aruco_marker_y, 
+    #     alpha=0.25)
+    fore_img = overlay_transparent_fore_alpha(fore_img, strip_img)
     fore_img = cv2.cvtColor(fore_img, cv2.COLOR_RGB2RGBA)
-    aruco_marker_x = int(fore_img.shape[1]/2 - aruco_img.shape[1]/2) + random.randint(-int(target_size/10), int(target_size/10))
-    aruco_marker_y = int(fore_img.shape[0]/2 - aruco_img.shape[0]/2) + random.randint(-int(target_size/10), int(target_size/10))
-    fore_img = combine_two_color_images(
-        fore_img, 
-        aruco_img, 
-        aruco_marker_x,
-        aruco_marker_y, 
-        alpha=0.25)
-    #cv2.imwrite(str(root_dir / 'test2.png'), fore_img)
-    # Set fore img alpha to 255
     fore_img[:, :, 3] = 255
+    cv2.imwrite(str(root_dir / 'fore_img.png'), fore_img)
+    # Set fore img alpha to 255
+    
     
     # Create poly in fore img coord system
     label_poly = Polygon([(aruco_marker_x, aruco_marker_y), (aruco_marker_x + aruco_img_size, aruco_marker_y), (aruco_marker_x + aruco_img_size, aruco_marker_y + aruco_img_size), (aruco_marker_x, aruco_marker_y + aruco_img_size)])
@@ -63,10 +76,11 @@ for i in range(num_pics_to_gen):
     for mat in mats:
         final_mat = final_mat @ mat
     fore_img, mat_label_polys = homogeneous_mat_transform(fore_img, [label_poly], img_size, final_mat, border_type=cv2.BORDER_TRANSPARENT)
-    #print(fore_img.shape)
     # -------------------------------------------------------------------------------------------------------------------------------------------
     
-    img = overlay_transparent(back_img, fore_img, int(back_img.shape[1]/2 - fore_img.shape[1]/2), int(back_img.shape[0]/2 - fore_img.shape[0]/2))
+    print('shapes:', back_img.shape, fore_img.shape)
+    print(int(back_img.shape[1]/2 - fore_img.shape[1]/2), int(back_img.shape[0]/2 - fore_img.shape[0]/2))
+    img = overlay_transparent_fore_alpha(back_img, fore_img)
     
     cv2.imwrite(str(dataseries_dir / f'{i}_in.png'), img)
     
