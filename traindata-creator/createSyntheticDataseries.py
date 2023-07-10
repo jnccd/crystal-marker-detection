ca6752e82aa21d0616d8b465d477b6795118c152
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-07-10 23:54:20 +0200

    Refactor

--- traindata-creator/createSyntheticDataseries.py
+++ traindata-creator/createSyntheticDataseries.py
@@ -25,37 +25,30 @@ fore_textures = [cv2.imread(str(p), -1) for p in get_files_from_folders_with_end
 for i in range(num_pics_to_gen):
     img_size = (target_size, target_size)
     
+    # Init pics
     back_img = center_crop_to_11_aspect_ratio(back_textures[i % len(back_textures)], target_size)
     fore_img = center_crop_to_11_aspect_ratio(fore_textures[i % len(fore_textures)], target_size)
     
+    # Setup aruco marker pos
     aruco_marker_x = int(fore_img.shape[1]/2 - aruco_img.shape[1]/2) + random.randint(-int(target_size/10), int(target_size/10))
     aruco_marker_y = int(fore_img.shape[0]/2 - aruco_img.shape[0]/2) + random.randint(-int(target_size/10), int(target_size/10))
     
-    strip_img = np.zeros((target_size, target_size) + (3,), dtype = np.uint8)
-    strip_img = cv2.cvtColor(strip_img, cv2.COLOR_RGB2RGBA)
+    # Build strip image
     strip_brightness = random.randrange(50, 100)
     strip_width = int(target_size/5)
     strip_opacity = 100
+    strip_img = np.zeros((target_size, target_size) + (3,), dtype = np.uint8)
+    strip_img = cv2.cvtColor(strip_img, cv2.COLOR_RGB2RGBA)
     strip_img[:, :, 3] = 0
     cv2.rectangle(strip_img, (0, int(target_size/2 - strip_width/2)), (target_size, int(target_size/2 + strip_width/2)), (strip_brightness, strip_brightness, strip_brightness, strip_opacity), -1)
-    strip_img[aruco_marker_y:aruco_marker_y+aruco_img.shape[0], aruco_marker_x:aruco_marker_x+aruco_img.shape[1], 3] = aruco_img[:, :, 0].astype('float32') / 255 * 100
-    cv2.imwrite(str(root_dir / 'strip_img.png'), strip_img)
+    strip_img[aruco_marker_y:aruco_marker_y+aruco_img.shape[0], aruco_marker_x:aruco_marker_x+aruco_img.shape[1], 3] = aruco_img[:, :, 0].astype('float32') / 255 * strip_opacity
+    #cv2.imwrite(str(root_dir / 'strip_img.png'), strip_img)
     
-    # Add aruco img to fore img and add alpha channel
-    #fore_img = overlay_transparent(fore_img, aruco_img, int(fore_img.shape[1]/2 - aruco_img.shape[1]/2), int(fore_img.shape[0]/2 - aruco_img.shape[0]/2))
-    #fore_img = cv2.addWeighted(fore_img,0.4,aruco_img,0.1,0)
-    # fore_img = combine_two_color_images(
-    #     fore_img, 
-    #     aruco_img, 
-    #     aruco_marker_x,
-    #     aruco_marker_y, 
-    #     alpha=0.25)
+    # Add strip image to fore img
     fore_img = overlay_transparent_fore_alpha(fore_img, strip_img)
     fore_img = cv2.cvtColor(fore_img, cv2.COLOR_RGB2RGBA)
     fore_img[:, :, 3] = 255
-    cv2.imwrite(str(root_dir / 'fore_img.png'), fore_img)
-    # Set fore img alpha to 255
-    
+    #cv2.imwrite(str(root_dir / 'fore_img.png'), fore_img)
     
     # Create poly in fore img coord system
     label_poly = Polygon([(aruco_marker_x, aruco_marker_y), (aruco_marker_x + aruco_img_size, aruco_marker_y), (aruco_marker_x + aruco_img_size, aruco_marker_y + aruco_img_size), (aruco_marker_x, aruco_marker_y + aruco_img_size)])
