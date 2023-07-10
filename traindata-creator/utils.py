ff1c1548f5fc853366eb07b8018e62eb20531baf
jnccd <kobidogao@outlook.com>
2023-07-10 02:33:06 +0200

    Always copy img_to_overlay_t

--- traindata-creator/utils.py
+++ traindata-creator/utils.py
@@ -182,43 +182,45 @@ def create_random_persp_mat(img_size_wh, perspective_strength = 0.3):
 
 # Taken from https://gist.github.com/clungzta/b4bbb3e2aa0490b0cfcbc042184b0b4e
 def overlay_transparent(background_img, img_to_overlay_t, x, y, overlay_size=None):
-	"""
-	@brief      Overlays a transparant PNG onto another image using CV2
+    """
+    @brief      Overlays a transparant PNG onto another image using CV2
 	
-	@param      background_img    The background image
-	@param      img_to_overlay_t  The transparent image to overlay (has alpha channel)
-	@param      x                 x location to place the top-left corner of our overlay
-	@param      y                 y location to place the top-left corner of our overlay
-	@param      overlay_size      The size to scale our overlay to (tuple), no scaling if None
+    @param      background_img    The background image
+    @param      img_to_overlay_t  The transparent image to overlay (has alpha channel)
+    @param      x                 x location to place the top-left corner of our overlay
+    @param      y                 y location to place the top-left corner of our overlay
+    @param      overlay_size      The size to scale our overlay to (tuple), no scaling if None
 	
-	@return     Background image with overlay on top
-	"""
-	
-	bg_img = background_img.copy()
-	
-	if overlay_size is not None:
-		img_to_overlay_t = cv2.resize(img_to_overlay_t.copy(), overlay_size)
-
-	# Extract the alpha mask of the RGBA image, convert to RGB 
-	b,g,r,a = cv2.split(img_to_overlay_t)
-	overlay_color = cv2.merge((b,g,r))
+    @return     Background image with overlay on top
+    """
+    
+    bg_img = background_img.copy()
+    
+    if overlay_size is not None:
+        img_to_overlay_t = cv2.resize(img_to_overlay_t.copy(), overlay_size)
+    else:
+        img_to_overlay_t = img_to_overlay_t.copy()
+        
+    # Extract the alpha mask of the RGBA image, convert to RGB 
+    b,g,r,a = cv2.split(img_to_overlay_t)
+    overlay_color = cv2.merge((b,g,r))
 	
 	# Apply some simple filtering to remove edge noise
-	mask = cv2.medianBlur(a,5)
+    mask = cv2.medianBlur(a,5)
 
-	h, w, _ = overlay_color.shape
-	roi = bg_img[y:y+h, x:x+w]
+    h, w, _ = overlay_color.shape
+    roi = bg_img[y:y+h, x:x+w]
 
 	# Black-out the area behind the logo in our original ROI
-	img1_bg = cv2.bitwise_and(roi.copy(),roi.copy(),mask = cv2.bitwise_not(mask))
+    img1_bg = cv2.bitwise_and(roi.copy(),roi.copy(),mask = cv2.bitwise_not(mask))
 	
 	# Mask out the logo from the logo image.
-	img2_fg = cv2.bitwise_and(overlay_color,overlay_color,mask = mask)
+    img2_fg = cv2.bitwise_and(overlay_color,overlay_color,mask = mask)
 
 	# Update the original image with our new ROI
-	bg_img[y:y+h, x:x+w] = cv2.add(img1_bg, img2_fg)
+    bg_img[y:y+h, x:x+w] = cv2.add(img1_bg, img2_fg)
 
-	return bg_img
+    return bg_img
 
 # Taken from https://stackoverflow.com/questions/48979219/opencv-composting-2-images-of-differing-size and modified
 def combine_two_color_images(back_img, fore_img, x = 0, y = 0, alpha = 0.5):
