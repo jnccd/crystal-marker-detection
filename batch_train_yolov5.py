45f2082b078338ed2a58cc0638ea193b2150af7a
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-07-13 22:01:24 +0200

    Add valset-path arg to yolov5 batch train

--- batch_train_yolov5.py
+++ batch_train_yolov5.py
@@ -10,21 +10,23 @@ start_time = time.time()
 
 parser = argparse.ArgumentParser(prog='', description='.')
 parser.add_argument('-d','--dataset-path', type=str, default='', help='.')
+parser.add_argument('-v','--valset-path', type=str, default='', help='.')
 args = parser.parse_args()
 
 dataset_dirs = [x for x in Path(args.dataset_path).glob('**/yolov5-*/') 
                 if x.is_dir() 
                     and not str(x).__contains__("_old") 
                     and not str(x).__contains__("-valset")]
+valset_dir = Path(args.valset_path)
 #print(dataset_dirs)
 
 for dir in dataset_dirs[:1]:
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-1 -d {dir} -e 300 --no-aug')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-2 -d {dir} -e 300 --no-aug')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-3 -d {dir} -e 300 --no-aug')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-1 -d {dir} -e 300')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-2 -d {dir} -e 300')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-3 -d {dir} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-1 -d {dir} -v {valset_dir} -e 300 --no-aug')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-2 -d {dir} -v {valset_dir} -e 300 --no-aug')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-3 -d {dir} -v {valset_dir} -e 300 --no-aug')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-1 -d {dir} -v {valset_dir} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-2 -d {dir} -v {valset_dir} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-3 -d {dir} -v {valset_dir} -e 300')
     
 end_time = time.time()
 diff_time = end_time  - start_time
