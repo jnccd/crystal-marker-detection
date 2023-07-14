8b0dba19f69f5f64ab871b1f4b1e8134ad1c4179
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-07-14 18:26:29 +0200

    Rename valset in yolov5 batch train script to testset

--- batch_train/yolov5.py
+++ batch_train/yolov5.py
@@ -10,7 +10,7 @@ start_time = time.time()
 
 parser = argparse.ArgumentParser(prog='', description='.')
 parser.add_argument('-d','--dataset-path', type=str, default='', help='.')
-parser.add_argument('-v','--valset-path', type=str, default='', help='.')
+parser.add_argument('-v','--testset-path', type=str, default='', help='.')
 args = parser.parse_args()
 
 root_dir = Path(__file__).resolve().parent
@@ -19,17 +19,17 @@ dataset_dirs = [x for x in dataset_path.glob('**/yolov5-*/')
                 if x.is_dir() 
                     and not str(x).__contains__("_old") 
                     and not str(x).__contains__("-valset")]
-valset_path = root_dir.parent / args.valset_path
+testset_path = root_dir.parent / args.valset_path
 newline_char = "\n" # Python 3.9 :/
 print(f'Running ensample run using the following datasets:\n{newline_char.join([str(x) for x in dataset_dirs])}')
 
 for dir in dataset_dirs:
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-1 --no-aug -d {dir} -v {valset_path} -e 300')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-2 --no-aug -d {dir} -v {valset_path} -e 300')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-3 --no-aug -d {dir} -v {valset_path} -e 300')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-1 -d {dir} -v {valset_path} -e 300')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-2 -d {dir} -v {valset_path} -e 300')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-3 -d {dir} -v {valset_path} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-1 --no-aug -d {dir} -v {testset_path} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-2 --no-aug -d {dir} -v {testset_path} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-3 --no-aug -d {dir} -v {testset_path} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-1 -d {dir} -v {testset_path} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-2 -d {dir} -v {testset_path} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-3 -d {dir} -v {testset_path} -e 300')
     
 end_time = time.time()
 diff_time = end_time  - start_time
