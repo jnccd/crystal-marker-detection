d2654eb247d931c11f06e38993adb2043cf812bd
jnccd <kobidogao@outlook.com>
2023-07-14 13:02:33 +0200

    Refactor

--- batch_train/yolov5.py
+++ batch_train/yolov5.py
@@ -24,9 +24,9 @@ newline_char = "\n" # Python 3.9 :/
 print(f'Running ensample run using the following datasets:\n{newline_char.join([str(x) for x in dataset_dirs])}')
 
 for dir in dataset_dirs[:1]:
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-1 -d {dir} -v {valset_path} -e 300 --no-aug')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-2 -d {dir} -v {valset_path} -e 300 --no-aug')
-    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-3 -d {dir} -v {valset_path} -e 300 --no-aug')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-1 --no-aug -d {dir} -v {valset_path} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-2 --no-aug -d {dir} -v {valset_path} -e 300')
+    os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-3 --no-aug -d {dir} -v {valset_path} -e 300')
     os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-1 -d {dir} -v {valset_path} -e 300')
     os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-2 -d {dir} -v {valset_path} -e 300')
     os.system(f'python repos/yolov5_train_loop.py -n {dir.stem}-yolo5aug-3 -d {dir} -v {valset_path} -e 300')
