8852292bdae892212996ce4a75e5909e40ad8f4e
jnccd <kobidogao@outlook.com>
2023-07-14 18:26:03 +0200

    Add batch train utils

---
+++ batch_train/utils.py
@@ -0,0 +1,9 @@
+
+def read_textfile(tf_path):
+    with open(tf_path, 'r') as file:
+        file_text = file.read()
+    return file_text
+
+def write_textfile(text, tf_path):
+    with open(tf_path, "w") as text_file:
+        text_file.write(text)
