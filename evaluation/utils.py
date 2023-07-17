57c60b7333560507861d9dded15886f952266a5b
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-07-17 19:23:00 +0200

    Dont annotate 0 bars

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -41,11 +41,12 @@ def autolabel(rects, ax):
     """Attach a text label above each bar in *rects*, displaying its height."""
     for rect in rects:
         height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
+        if height > 0:
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
 
 # --- Textfiles -------------------------------------------------------------------------------------------------------------------------
 
