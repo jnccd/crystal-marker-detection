d601bec6286cc789347add994a9071db1681ad16
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-07-17 19:23:54 +0200

    Add plot name, increase bar width, increase plot size/dpi, shorten labels, set target folder

--- evaluation/plot_ensample.py
+++ evaluation/plot_ensample.py
@@ -12,9 +12,11 @@ from matplotlib import pyplot as plt
 from utils import *
 
 parser = argparse.ArgumentParser(prog='', description='.')
+parser.add_argument('-n','--name', type=str, help='.')
 parser.add_argument('-rf','--runs-folders', action='append', nargs='+', type=str, help='.')
 args = parser.parse_args()
 
+root_dir = Path(__file__).resolve().parent
 runs_folders = flatten(args.runs_folders)
 eval_paths = flatten([[ x for x in Path(run_folders).glob('**/evals.json') 
                         if not str(x).__contains__("_old")] for run_folders in runs_folders])
@@ -37,7 +39,7 @@ for eval_path in eval_paths:
     
     run_name: str = train_def_dict['run_name']
     if run_name.endswith('-1'):
-        bar_chart_labels.append('-'.join(run_name.split('-')[4:]))
+        bar_chart_labels.append('-'.join(run_name.split('-')[4:-1]))
         bar_chart_voc2007_mAPs.append(float(eval_dict['voc2007_mAP']))
         bar_chart_voc2010_mAPs.append(float(eval_dict['voc2010_mAP']))
         bar_chart_coco_mAPs.append(float(eval_dict['coco_mAP']))
@@ -45,16 +47,16 @@ for eval_path in eval_paths:
 bar_chart_voc2007_mAPs = [round(x, 3) for x in bar_chart_voc2007_mAPs]
 bar_chart_voc2010_mAPs = [round(x, 3) for x in bar_chart_voc2010_mAPs]
 bar_chart_coco_mAPs = [round(x, 3) for x in bar_chart_coco_mAPs]
-        
+
 # Create barchart
 x = np.arange(len(bar_chart_labels))
-width = 0.35 / 3
+width = 0.6 / 3
 fig, ax = plt.subplots()
 v7_bars = ax.bar(x - width, bar_chart_voc2007_mAPs, width, label='voc2007 mAP')
 v10_bars = ax.bar(x, bar_chart_voc2010_mAPs, width, label='voc2010 mAP')
 coco_bars = ax.bar(x + width, bar_chart_coco_mAPs, width, label='coco mAP')
 ax.set_ylabel('mAP')
-ax.set_title('mAP per run')
+ax.set_title(f'mAP per run in {args.name.replace("-", " ")}')
 ax.set_xticks(x)
 ax.set_xticklabels(bar_chart_labels, rotation=30, ha='right')
 ax.legend()
@@ -62,4 +64,5 @@ autolabel(v7_bars, ax)
 autolabel(v10_bars, ax)
 autolabel(coco_bars, ax)
 fig.tight_layout()
-plt.show()
+plt.gcf().set_size_inches(20, 9)
+plt.savefig(root_dir / f'{args.name}.pdf', dpi=300)
