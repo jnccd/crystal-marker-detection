fef8bf76ce12db6472c89f56458e9a2d8c5f312b
jnccd <kobidogao@outlook.com>
2023-07-18 20:23:34 +0200

    Move init to root

--- init.sh
+++ init.sh
@@ -3,14 +3,16 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
+cd repos
+
 # Yolov5
 git clone https://github.com/ultralytics/yolov5
 cd yolov5
 pip install -r requirements.txt
 cd ..
 
-# Retinanet Model
-git clone https://github.com/fizyr/keras-retinanet
-cd keras-retinanet
-pip install . --user
-cd ..
+# # Retinanet Model
+# git clone https://github.com/fizyr/keras-retinanet
+# cd keras-retinanet
+# pip install . --user
+# cd ..
