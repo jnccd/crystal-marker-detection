a07b05f3a9b86916a5beae768a2f846ea5ab322e
jnccd <kobidogao@outlook.com>
2023-07-22 16:42:56 +0200

    Move utility funs of yolov5 gen eval data to utils

--- repos/yolov5_gen_evaldata.py
+++ repos/yolov5_gen_evaldata.py
@@ -10,6 +10,8 @@ import cv2
 from pathlib import Path
 import pandas as pd
 
+from utils import *
+
 def main():
     parser = argparse.ArgumentParser(prog='yolov5-gen-evaluation-data', description='Generate testable evaluation data for yolov5 output on some datasets valdata.')
     parser.add_argument('-r','--run', type=str, default='', help='Yolov5 run foldername.')
@@ -96,37 +98,6 @@ def main():
                 cv2.imwrite(str(out_testdata_path / f'{i}_target_output.png'), sanity_check_image)
                 
         i+=1
-
-def get_files_from_folders_with_ending(folders, ending):
-    paths = []
-    for folder in folders:
-        paths.extend(sorted(
-            [
-                os.path.join(folder, fname)
-                for fname in os.listdir(folder)
-                if fname.endswith(ending)
-            ]
-        ))
-    return paths
-
-def set_img_width(img, max_width):
-    img_h, img_w = img.shape[:2]
-    resize_factor = float(max_width) / img_w
-    target_size = (max_width, int(img_h * resize_factor))
-    return cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
-
-def set_img_height(img, max_height):
-    img_h, img_w = img.shape[:2]
-    resize_factor = float(max_height) / img_h
-    target_size = (int(img_w * resize_factor), max_height)
-    return cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
-
-def create_dir_if_not_exists(dir: Path, clear = False):
-    if clear and os.path.isdir(dir):
-        shutil.rmtree(dir)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    return dir
 
 if __name__ == '__main__':
     main()
