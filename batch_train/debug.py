3cd6cd0b864997193d5f02e1516309574e8bad13
jnccd <kobidogao@outlook.com>
2023-07-22 19:07:37 +0200

    Add comment

--- batch_train/debug.py
+++ batch_train/debug.py
@@ -6,4 +6,5 @@ model = YOLO('../training/yolov8/yolov5-640-good-pics-v1-no-aug/weights/best.pt'
 valset_path = pathlib.Path('../traindata-creator/dataset/yolov5-640-on-skin-valset-v2/')
 out_testdata_path = pathlib.Path('../training/yolov8/yolov5-640-good-pics-v1-no-aug/test')
 
+# Invoke directly
 yolov8.gen_evaldata(model, valset_path, out_testdata_path)
