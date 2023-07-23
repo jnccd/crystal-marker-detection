abef7402cd55e5d8a9c3a616788cc440c10921e2
jnccd <kobidogao@outlook.com>
2023-07-23 18:12:12 +0200

    Add reeval script

---
+++ evaluation/reeval.py
@@ -0,0 +1,26 @@
+import argparse
+import ast
+import json
+import os
+import sys
+import time
+from pathlib import Path
+
+from utils import *
+
+# Parse
+parser = argparse.ArgumentParser(prog='', description='Regenerate evaldata and reanalyze outputs, in case there was a bug in the prod server script.')
+parser.add_argument('-r','--runs-path', type=str, default='', help='.')
+parser.add_argument('-v','--testset-path', type=str, default='', help='.')
+parser.add_argument('-t','--run-type', type=str, default='yolov5', help='.')
+args = parser.parse_args()
+
+run_test_dirs = [x.parent for x in Path(args.runs_path).glob('**/training-def.json')]
+
+for training_run_folder in run_test_dirs:
+    if args.run_type == 'yolov5':
+        os.system(f'python repos/yolov5_gen_evaldata.py -r {training_run_folder.parent} -df {args.testset_path}/')
+        os.system(f'python evaluation/analyze.py -av {training_run_folder}')
+    elif args.run_type == 'yolov8':
+        # TODO: Import somehow?
+        os.system(f'python evaluation/analyze.py -av {training_run_folder}')
