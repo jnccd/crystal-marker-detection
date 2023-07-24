b3d6e85a88b400bf5f1244f1ea0e573dfbf49bfc
jnccd <kobidogao@outlook.com>
2023-07-24 02:15:18 +0200

    Automatically call fixYolo5Yamls on yolov5 batch train

--- batch_train/yolov5.py
+++ batch_train/yolov5.py
@@ -35,6 +35,8 @@ def main():
     newline_char = "\n" # Python 3.9 :/
     print(f'Running ensample run using the following datasets:\n{newline_char.join([str(x) for x in dataset_dirs])}')
     
+    os.system(f'python traindata-creator/fixYolo5Yamls -df {dataset_path}')
+    
     # Train
     start_time = time.time()
 
