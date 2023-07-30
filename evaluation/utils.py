08ec83a2e47a8784865e63990bea8951c4ab3583
jnccd <kobidogao@outlook.com>
2023-07-30 18:39:16 +0200

    Remove unnecessary imports from eval utils

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -3,10 +3,6 @@ from pathlib import Path
 import shutil
 from typing import List
 
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-
 # String stuff
 def startswith_any(filename: str, prefs: List[str]):
     for pref in prefs:
