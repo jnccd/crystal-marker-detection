0c2229a9b23f1bb6d0ea2167ffc06b0778f0b83e
jnccd <kobidogao@outlook.com>
2023-07-31 18:49:15 +0200

    Move rounding before filter

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -36,9 +36,9 @@ def flatten(list):
 def autolabel(rects, ax):
     """Attach a text label above each bar in *rects*, displaying its height."""
     for rect in rects:
-        height = rect.get_height()
+        height = round(rect.get_height(), 3)
         if height > 0:
-            ax.annotate('{}'.format(round(height, 3)),
+            ax.annotate('{}'.format(height),
                         xy=(rect.get_x() + rect.get_width() / 2, height),
                         xytext=(0, 3),  # 3 points vertical offset
                         textcoords="offset points",
