c815c4d3c67a0fcd46b343de55c9e13cf7dbb3d3
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-08-01 15:13:32 +0200

    Try out python code

--- mip_worker_batch_train.py
+++ mip_worker_batch_train.py
@@ -9,6 +9,6 @@ parser.add_argument('-i','--image', type=str, default='ncarstensen/pcmd:0.1', he
 args = parser.parse_args()
 
 for i in range(args.num_gpus):
-    print_str = f"Starting run {i} of {args.num_gpus}"
+    print_str = f"Starting run {i} of {args.num_gpus} "
     print(print_str)
     os.system(f"screen -dmS worker{i} with_gpu -n 1 sudo mip-docker-run --rm --gpus '\"device=$CUDA_VISIBLE_DEVICES\"' {args.image} {args.command} -wi {i} -wc {args.num_gpus}")
