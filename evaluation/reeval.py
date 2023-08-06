290ba979777aa98538945bac07c1e489d403db12
jnccd <kobidogao@outlook.com>
2023-08-06 20:07:00 +0200

    Add automated run type choosing in reeval

--- evaluation/reeval.py
+++ evaluation/reeval.py
@@ -2,6 +2,7 @@ import argparse
 import ast
 import json
 import os
+import re
 import sys
 import time
 from pathlib import Path
@@ -10,17 +11,42 @@ from utils import *
 
 # Parse
 parser = argparse.ArgumentParser(prog='', description='Regenerate evaldata and reanalyze outputs, in case there was a bug in the prod server script.')
-parser.add_argument('-r','--runs-path', type=str, default='', help='.')
-parser.add_argument('-v','--testset-path', type=str, default='', help='.')
-parser.add_argument('-t','--run-type', type=str, default='yolov5', help='.')
+parser.add_argument('-r','--runs-path', type=str, help='.')
+parser.add_argument('-t','--testset-path', type=str, help='.')
+parser.add_argument('-rt','--run-type', type=str, help='.')
 args = parser.parse_args()
 
-run_dirs = [x.parent.parent for x in Path(args.runs_path).glob('**/training-def.json')]
+run_dirs = [(x.parent.parent, x) for x in Path(args.runs_path).glob('**/training-def.json')]
+newline_char = "\n" # Python 3.9 :/
+print(f'Running reeval run on the following {len(run_dirs)} training runs:\n{newline_char.join([str(x[0]) for x in run_dirs])}')
 
-for training_run_folder in run_dirs:
-    if args.run_type == 'yolov5':
+yolov5_pattern = re.compile('yolov5(.?)$')
+yolov8_pattern = re.compile('yolov8(.?)$|yolov5(.?)u$') 
+
+for training_run_folder, train_def_path in run_dirs:
+    # Read train def
+    train_def_json = json.loads(read_textfile(train_def_path).replace("    ", "").replace("\n", ""))
+    train_def_model = train_def_json['model']
+    
+    # Set run_type
+    if args.run_type is not None:
+        current_run_type = args.run_type
+    else:
+        if yolov5_pattern.match(train_def_model):
+            current_run_type = 'yolov5'
+        elif yolov8_pattern.match(train_def_model):
+            current_run_type = 'yolov8'
+        else:
+            print('What model is that? ' + train_def_model)
+            sys.exit(1)
+            
+    # Regenerate
+    print(current_run_type)
+    os.system(f'rm {training_run_folder / "test" / "*.txt"}'.replace("\\", "/"))
+    os.system(f'rm {training_run_folder / "test" / "*.png"}'.replace("\\", "/"))
+    if current_run_type == 'yolov5':
         os.system(f'python repos/yolov5_gen_evaldata.py -r {training_run_folder} -df {args.testset_path}/')
         os.system(f'python evaluation/analyze.py -av {training_run_folder}')
-    elif args.run_type == 'yolov8':
+    elif current_run_type == 'yolov8':
         os.system(f'python batch_train/yolov8_gen_evaldata.py -r {training_run_folder} -df {args.testset_path}/')
         os.system(f'python evaluation/analyze.py -av {training_run_folder}')
