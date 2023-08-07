cec96073a6712a0a111884029d1b4d7f4a126455
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-08-07 04:35:40 +0200

    Try yolov5 noise again

--- prototype-yolo/noise_models/create_noise_yolo.py
+++ prototype-yolo/noise_models/create_noise_yolo.py
@@ -10,12 +10,14 @@ def main():
     os.environ['TORCH_HOME'] = './.cache'
 
     for i in range(2):
-        model = YOLO('batch_train/models/yolov5s.pt').model
-        #model = torch.hub.load('ultralytics/yolov5', 'custom', path=f'yolov5s.pt')
+        #model = YOLO('batch_train/models/yolov5s.pt').model
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path=f'yolov5s.pt')
         #next(model.parameters()).to(torch.device("cuda:0"))
         with torch.no_grad():
             for param in model.parameters():
-                param.add_(torch.randn(param.size()) * 0.03)
+                param.to(torch.device("cuda:0"))
+                print(param, param.device == torch.device("cuda:0"))
+                param[3][0].add(torch.randn(param.size()[2:]) * 0.03)
         torch.save(model, root_dir / f'test_noise_{i}.pt')
     
     #os.system('rm *.pt')
