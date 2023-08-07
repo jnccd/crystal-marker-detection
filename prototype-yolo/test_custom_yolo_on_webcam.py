b6c4c7c6f9ae21c40d80cd9c3107f9f9c5d9cb52
jnccd <kobidogao@outlook.com>
2023-08-07 03:41:48 +0200

    Update prototype script

--- prototype-yolo/test_custom_yolo_on_webcam.py
+++ prototype-yolo/test_custom_yolo_on_webcam.py
@@ -5,14 +5,12 @@ import numpy as np
 import cv2
 from pathlib import Path
 
-parser = argparse.ArgumentParser(prog='tes-stuff', description='test stuff.')
-parser.add_argument('-r','--run', type=str, default='exp6', help='Yolov5 run foldername.')
+parser = argparse.ArgumentParser(prog='test-stuff', description='test stuff.')
+parser.add_argument('-r','--run-path', type=str, help='.')
 args = parser.parse_args()
 
-run = args.run
-
 root_dir = Path(__file__).resolve().parent
-network_file = str(root_dir / f'../repos/yolov5/runs/train/{run}/weights/best.pt')
+network_file = str(root_dir / '..' / args.run_path)
 
 print("network_file",network_file)
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=network_file, force_reload=True)
