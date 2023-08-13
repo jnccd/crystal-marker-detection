2a66d6f4b541182d9db3deff34eb65b18ebaf4e1
jnccd <kobidogao@outlook.com>
2023-08-14 00:22:48 +0200

    Add use-sahi option to yolov5 gen evaldata

--- repos/yolov5_gen_evaldata.py
+++ repos/yolov5_gen_evaldata.py
@@ -16,8 +16,9 @@ def main():
     parser = argparse.ArgumentParser(prog='yolov5-gen-evaluation-data', description='Generate testable evaluation data for yolov5 output on some datasets valdata.')
     parser.add_argument('-r','--run', type=str, default='', help='Yolov5 run foldername, or path to runfolder.')
     parser.add_argument('-df','--dataset-folder', type=str, default='',  help='The trainings data folder name to learn from or build into.')
+    parser.add_argument('-us','--use-sahi', action='store_true', help='Use Sahi for inference.')
     args = parser.parse_args()
-
+    
     # Get chosen or last yolov5 run dir
     root_dir = Path(__file__).resolve().parent
     train_dir = root_dir / f'../training/yolov5'
@@ -40,10 +41,16 @@ def main():
     # Torch hub cache support on
     os.system('mkdir ./.cache')
     os.environ['TORCH_HOME'] = './.cache'
-
+    
     print("network_file:",network_file)
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=network_file)
-
+    if not args.use_sahi:
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path=network_file)
+    else:
+        # Get sahi imports and model if set
+        from sahi.predict import get_sliced_prediction
+        from sahi import AutoDetectionModel
+        model = AutoDetectionModel.from_pretrained(model_type='yolov5', model_path=network_file)
+    
     valdata_imgs_path = Path(args.dataset_folder) / 'val/images'
     valdata_labels_path = Path(args.dataset_folder) / 'val/labels'
     
@@ -57,20 +64,40 @@ def main():
         # Write input picture
         shutil.copyfile(img_path, out_testdata_path / f'{i}_input.png')
         
-        # Write model out
         img = cv2.imread(img_path)
         img_h, img_w = img.shape[:2]
-        results = model(img)
-        res_df = results.pandas().xyxy[0]
-        out_path = out_testdata_path / f'{i}_network_output.txt'
-        with open(out_path, "w") as text_file:
-            res_df = res_df.reset_index()
-            for index, row in res_df.iterrows():
-                if row['confidence'] > 0.5:
-                    text_file.write(f"{row['xmin']} {row['ymin']} {row['xmax']} {row['ymax']} {row['confidence']}\n")
-        #print(out_img_path)
-        cv2.imwrite(str(out_testdata_path / f'{i}_result_render.png'), np.squeeze(results.render()))
         
+        if not args.use_sahi:
+            # Default Yolov5 inference
+            
+            # Write model out
+            results = model(img)
+            res_df = results.pandas().xyxy[0]
+            out_path = out_testdata_path / f'{i}_network_output.txt'
+            with open(out_path, "w") as text_file:
+                res_df = res_df.reset_index()
+                for index, row in res_df.iterrows():
+                    if row['confidence'] > 0.5:
+                        text_file.write(f"{row['xmin']} {row['ymin']} {row['xmax']} {row['ymax']} {row['confidence']}\n")
+            #print(out_img_path)
+            cv2.imwrite(str(out_testdata_path / f'{i}_result_render.png'), np.squeeze(results.render()))
+        else:
+            # Sahi inference
+            result = get_sliced_prediction(
+                img_path,
+                model,
+                slice_height = 640,
+                slice_width = 640,
+                overlap_height_ratio = 0.2,
+                overlap_width_ratio = 0.2
+            )
+            result.export_visuals(export_dir=str(out_testdata_path), file_name=f'{i}_result_render')
+            out_path = out_testdata_path / f'{i}_network_output.txt'
+            with open(out_path, "w") as text_file:
+                for pred in result.object_prediction_list:
+                    if pred.score.value > 0.5:
+                        text_file.write(f"{pred.bbox.minx} {pred.bbox.miny} {pred.bbox.maxx} {pred.bbox.maxy} {pred.score.value}\n")
+            
         # Write labels
         # Rasterize Segmentation image
         sanity_check_image = np.zeros((img_w, img_h) + (3,), dtype = np.uint8)
