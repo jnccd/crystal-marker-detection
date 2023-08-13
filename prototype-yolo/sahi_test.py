7083d5c3128dfb88202c3e1b94355f416a189f9b
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-08-13 20:37:46 +0200

    Test sahi

---
+++ prototype-yolo/sahi_test.py
@@ -0,0 +1,17 @@
+from sahi.predict import get_sliced_prediction
+from sahi import AutoDetectionModel
+
+detection_model = AutoDetectionModel.from_pretrained(model_type='yolov5', model_path='evaluation/from-server/yolov5s-rot-ensample/yolov5-640-gpv2-rot-234-p1-yolo5aug/weights/best.pt') # for YOLOv5 models
+
+# get sliced prediction result
+result = get_sliced_prediction(
+    'traindata-creator/dataseries/man-on_skin_v2/PXL_20230707_080009486_in.png',
+    detection_model,
+    slice_height = 640,
+    slice_width = 640,
+    overlap_height_ratio = 0.2,
+    overlap_width_ratio = 0.2
+)
+
+print(result)
+result.export_visuals('prototype-yolo/sahi_export/')
