f893f0357e8715cb2198cda92b7600569e578f5a
jnccd <kobidogao@outlook.com>
2023-08-26 00:24:20 +0200

    Expand desc

--- traindata-creator/createDataset.py
+++ traindata-creator/createDataset.py
@@ -28,7 +28,7 @@ def main():
     parser = argparse.ArgumentParser(prog='dataset-creator', description='Combines multiple dataseries into a dataset.')
     parser.add_argument('-n','--name', type=str, help='Defines the (folder)name of the dataset.')
     parser.add_argument('-t','--type', type=str, help='Defines the type of dataset to be build, "seg" for segmentation, "yolov5" for yolov5 od (object detection), "csv" for csv od.')
-    parser.add_argument('-s','--size', type=int, help='Defines the image size for the dataset.')
+    parser.add_argument('-s','--size', type=int, help='Defines the image size for the dataset, 0 for no resize.')
     parser.add_argument('-tf','--traindata-folders', action='append', nargs='+', type=str, help='The folders containing train data.')
     parser.add_argument('-vf','--valdata-folders', action='append', nargs='+', type=str, help='The folders containing validation data.')
     parser.add_argument('-taf','--target-folder', type=str, help='The folder to build the dataset folder into.')
