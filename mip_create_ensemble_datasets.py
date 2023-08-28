56af0e03f016a602ef83467139bad8179de7c7d3
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-08-29 01:51:42 +0200

    Add float_range support

--- mip_create_ensemble_datasets.py
+++ mip_create_ensemble_datasets.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 parser = argparse.ArgumentParser(prog='', description='.')
 parser.add_argument('-n','--name', type=str, default='test', help='.')
@@ -9,6 +10,15 @@ parser.add_argument('-np','--num-parts', type=int, default='5', help='.')
 parser.add_argument('-op','--other-params', type=str, default='', help='.')
 args = parser.parse_args()
 
-for ic in range(0, args.max_config, args.step_config):
+def float_range(start, stop, step):
+    nums = []
+    i = start
+    while i < stop:
+        nums.append(i)
+        i += step
+    nums.append(stop)
+    return [round(x, 10) for x in nums]
+
+for ic in float_range(0, args.max_config, args.step_config):
     for ip in range(args.num_parts):
         os.system(f'python traindata-creator/createDataset.py -n {args.name}-{ic}-p{ip} -a -aim 4 -agnc 1 {args.other_params} {ic} -taf /data/pcmd/dataset/_noise-ensemble-{args.name}')
