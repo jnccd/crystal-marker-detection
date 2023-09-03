aab39d96ed34a9fa2b387d734773d744e6fddce9
jnccd <kobidogao@outlook.com>
2023-09-03 06:24:13 +0200

    Fix dataset image size, Fix folder not being cleared, Fix training_subfolder choosing

--- hyperparam_search.py
+++ hyperparam_search.py
@@ -10,14 +10,14 @@ from evaluation.utility import *
 
 parser = argparse.ArgumentParser(prog='', description='.')
 parser.add_argument('-t','--testset-path', type=str, help='.')
-parser.add_argument('-df','--dataset-folder', type=str, default='traindata-creator/dataset', help='.') # /data/pcmd/dataset/
+parser.add_argument('-df','--dataset-folder', type=str, default='traindata-creator/dataset/_hyp-param-search', help='.') # /data/pcmd/dataset/hyp-param-search
 parser.add_argument('-tf','--training-folder', type=str, default='training/hyp-param-search', help='.') # /data/pcmd/training/hyp-param-search
 parser.add_argument('-ds','--dataseries-sources', type=str, default='-tf traindata-creator/dataseries/af-the_good_pics_for_nn2_s1/ traindata-creator/dataseries/af-the_good_pics_for_nn2_s2/ -r 0.2', help='.')
 args = parser.parse_args()
 
 testset_path = Path(args.testset_path)
-dataset_folder = Path(args.dataset_folder)
-training_folder = create_dir_if_not_exists(Path(args.training_folder))
+dataset_folder = create_dir_if_not_exists(Path(args.dataset_folder), clear=True)
+training_folder = create_dir_if_not_exists(Path(args.training_folder), clear=True)
 
 def_aug_params = [
     ("asgsc", 0, 1),
@@ -37,20 +37,22 @@ def_aug_params = [
 ]
 
 def hyp_param_run(param_dict):
+    training_folder = create_dir_if_not_exists(Path(args.training_folder), clear=True)
     
-    dataset_image_size = 0
+    dataset_image_size = 640
     dataset_name = 'hyp-search-set'
-    os.system(f'python traindata-creator/createDataset.py -n {dataset_name} -taf {dataset_folder} -s {dataset_image_size} -sd {param_dict["seed"]} {args.dataseries_sources} -t yolov5 -s 640 ' +\
+    os.system(f'python traindata-creator/createDataset.py -n {dataset_name} -taf {dataset_folder} -s {dataset_image_size} -sd {param_dict["seed"]} {args.dataseries_sources} -t yolov5 -s {dataset_image_size} ' +\
         '-a -aim 4 ' + ' '.join([f'-{x[0]} {param_dict[x[0]]}' for x in def_aug_params]))
     
-    os.system(f'python batch_train/yolov5.py -d {dataset_folder / dataset_name} -t {testset_path} -e {int(param_dict["epochs"])} -snr -o {training_folder}')
+    os.system(f'python batch_train/yolov5.py -d {dataset_folder} -t {testset_path} -e {int(param_dict["epochs"])} -snr -o {training_folder}')
     
-    eval_json_path = training_folder / f'yolov5-{dataset_image_size}-{dataset_name}' / 'test' / 'evals' / 'evals.json'
+    training_subfolder = [x.parent.stem for x in training_folder.glob('**/training-def.json')][0]
+    eval_json_path = training_folder / training_subfolder / 'test' / 'evals' / 'evals.json'
     eval_dict = json.loads(read_textfile(eval_json_path).replace("    ", "").replace("\n", ""))
     
     return {'loss': -eval_dict['voc2010_mAP'], 'status': STATUS_OK }
 
-space = dict([('epochs', hp.uniform('epochs', 25, 400)), 
+space = dict([('epochs', hp.uniform('epochs', 15, 35)), 
               ('seed', hp.randint('seed', sys.maxsize))] + 
              [(x[0], hp.uniform(x[0], x[1], x[2])) for x in def_aug_params])
 
