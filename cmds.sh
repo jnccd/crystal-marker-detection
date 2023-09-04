7d284086321ea7be7f386592d0f3432aa220c5f6
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-09-04 05:17:36 +0200

    Update cmds

--- cmds.sh
+++ cmds.sh
@@ -166,6 +166,7 @@ python evaluation/fusion_inference.py -r evaluation/from-server/noise-sgs-ensemb
 python evaluation/analyze.py -av evaluation/from-server/noise-sgs-ensemble/yolov5-640-gpv2-sgs-10-p0-yolo5aug/test_fused/
 python repos/yolov5_evaluate.py -r evaluation/from-server/yolov5s-rot-ensemble/yolov5-640-gpv2-rot-234-p1-yolo5aug/ -t traindata-creator/dataset/yolov5-0-on-skin-valset-v3-testset/ -us
 python repos/yolov5_evaluate.py -r evaluation/from-server/yolov5s-rot-ensemble/yolov5-640-gpv2-rot-234-p1-yolo5aug/ -t traindata-creator/dataset/yolov5-0-on-skin-valset-v3-ensemble-test/ -dbo -us -bis 0.2 -sqt 0.6
+python batch_train/yolov8_evaluate.py -r evaluation/from-server/yolov8s-runs/yolov5-640-good-pics-v2-slight-mat-only/ -t traindata-creator/dataset/yolov5-0-on-skin-valset-v3-ensemble-test/ -dbo -us -bis 0.2 -sqt 0.6 -tn test-script
 
 # Run datasets ensemble on remote
 python batch_train/yolov5.py -d /data/pcmd/dataset/sgss/ -t /data/pcmd/dataset/yolov5-640-on-skin-valset-v2/ -e 300 -o training/yolov5s-sgs-ensemble-test
