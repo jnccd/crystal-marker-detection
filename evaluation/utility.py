65b74c20387246781b814fd24af3010b1cb0796d
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-09-04 15:52:29 +0200

    Improve get_all_subfolder_run_dirs() output datastructure

--- evaluation/utility.py
+++ evaluation/utility.py
@@ -40,7 +40,8 @@ def get_all_subfolder_run_dirs(search_root_dirs):
       search_root_dirs: A list of paths to search all subfolders of for run files.
 
     Returns:
-      A list of dicts {'run_root', 'train_def', 'eval', [('test-name', test-def path, tests eval json path)]}.
+      A list of dicts for all found run folders [{'run_root', 'train_def', 'eval', {'$test_name': {'test_def', 'eval'} } }].
+      (The leafs are omitted)
     """
     
     train_def_paths = flatten([[x for x in Path(search_root_dir).glob('**/training-def.json')
@@ -50,9 +51,10 @@ def get_all_subfolder_run_dirs(search_root_dirs):
                     for x in train_def_paths]
     eval_paths = [x / 'test/evals/evals.json' for x in run_paths]
     test_defs_paths = [x.glob('**/_test-def.json') for x in run_paths]
-    tests_paths = [[(x.parent.stem, x, x.parent / 'evals/evals.json') for x in test_def_paths if os.path.isfile(x.parent / 'evals/evals.json')] for test_def_paths in test_defs_paths]
+    tests_paths = [[(x.parent.stem, {'test_def': x, 'eval': x.parent / 'evals/evals.json'}) for x in test_def_paths if os.path.isfile(x.parent / 'evals/evals.json')] for test_def_paths in test_defs_paths]
+    tests_path_dicts = [dict(x) if x != [] else {} for x in tests_paths]
     
-    tuple_paths_list = list(zip(run_paths, train_def_paths, eval_paths, tests_paths))
+    tuple_paths_list = list(zip(run_paths, train_def_paths, eval_paths, tests_path_dicts))
     dict_keys = ['run_root', 'train_def', 'eval', 'tests']
     dict_list = [dict(zip(dict_keys, x)) for x in tuple_paths_list 
                     if os.path.exists(x[2]) and os.path.isfile(x[2]) and os.path.exists(x[1]) and os.path.isfile(x[1])]
