591d2d3839cef090349f80617d9040c9f28e0ccb
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-09-05 15:08:03 +0200

    Add powershell commands for docker wsl2 clear

--- docker_cleanup.sh
+++ docker_cleanup.sh
@@ -3,3 +3,7 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker system prune
 docker system prune -af
+
+# Admin powershell
+#Optimize-VHD -Path "C:\Users\nikla\AppData\Local\Docker\wsl\data\ext4.vhdx" -Mode full
+#Optimize-VHD -Path "C:\Users\nikla\AppData\Local\Packages\CanonicalGroupLimited.Ubuntu_79rhkp1fndgsc\LocalState\ext4.vhdx" -Mode full
