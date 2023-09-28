215e43d9d1cb60e785fa63518dc30ce1d196677e
Niklas Carstensen <kobidogao@yahoo.com>
2023-09-28 22:15:41 +0200

    Update yolo nas test batch train command

--- cmds.sh
+++ cmds.sh
@@ -92,7 +92,7 @@ python batch_train/yolov5.py -d /data/pcmd/dataset/ -t /data/pcmd/dataset/yolov5
 python batch_train/yolov5.py -d /data/pcmd/dataset/noise-sgss/ -t /data/pcmd/dataset/yolov5-640-on-skin-valset-v2/ -o training/noise-sgs-ensemble -e 300
 python batch_train/yolov5.py -d traindata-creator/dataset/ -t traindata-creator/dataset/yolov5-640-on-skin-valset-v2/
 python batch_train/yolo_nas.py -e 2 -d traindata-creator/dataset/coco-640-best-yolov5s-hyp-search-set -rsf -t traindata-creator/dataset/yolov5-0-on-skin-valset-v3-ensemble-test/
-python batch_train/yolo_nas.py -e 2 -d /data/pcmd/dataset/coco-640-best-yolov5s-hyp-search-set -rsf -t /data/pcmd/dataset/yolov5-0-on-skin-valset-v3-ensemble-test/
+python batch_train/yolo_nas.py -e 200 -d /data/pcmd/dataset/coco-640-best-yolov5s-hyp-search-set -rsf -t /data/pcmd/dataset/yolov5-0-on-skin-valset-v3-ensemble-test/ -o /data/pcmd/training/yolo_nas/
 
 python -m cmd_tf -t -td traindata-creator/dataset/seg-640-on-skin-valset-v2/ -r sm-unet-aruco
 
