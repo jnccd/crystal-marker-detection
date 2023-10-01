f8cc93a97a3f3e855b16237bd15b1a09c5c9329d
jnccd <kobidogao@outlook.com>
2023-10-02 00:31:22 +0200

    Move thesis graphs into graph folder

--- thesis/assets/build.py
+++ thesis/assets/build.py
@@ -11,12 +11,13 @@ def read_textfile(tf_path):
     return file_text
 
 root_dir = Path(__file__).resolve().parent
-dot_files = list(root_dir.glob('*.dot'))
+graph_dir = root_dir / 'graph'
+dot_files = list(graph_dir.glob('*.dot'))
 
 print(f'building {len(dot_files)} graphs...')
 
 for dot_file in dot_files:
     ps = Source(read_textfile(dot_file), format='pdf')
-    ps.render(str(root_dir / dot_file.stem))
-    os.system(f'rm {root_dir / dot_file.stem}')
+    ps.render(str(graph_dir / dot_file.stem))
+    os.system(f'rm {graph_dir / dot_file.stem}')
     
