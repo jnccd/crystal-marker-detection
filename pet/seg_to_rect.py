e8467c70c5ebac88fc5a97640266711fd70f5b67
jnccd <kobidogao@outlook.com>
2023-10-27 17:32:38 +0200

    Use harris corners with large kernel for seg to rect

--- pet/seg_to_rect.py
+++ pet/seg_to_rect.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import random
 import cv2
+import numpy as np
 
 from utils import *
 
@@ -12,39 +13,24 @@ def main():
     
     pred_img_paths = [Path(x) for x in get_files_from_folders_with_ending([eval_folder], '_pred.png')]
     for pred_img_path in pred_img_paths:
-        img = cv2.imread(str(pred_img_path),0)
+        img = cv2.imread(str(pred_img_path))
         cv2.imwrite(str(to_rect_output_folder / f'{pred_img_path.stem}_pyr_img.png'), img)
 
-        # Apply edge detection
-        edges = cv2.Canny(img, threshold1=1, threshold2=100)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = np.float32(gray)
         
-        print(edges.shape)
-        print(edges)
-
-        # Find contours
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-        eps = 0.03
-        # draw the approximated contour on the image
-        output = img.copy()
-        #output = cv2.cvtColor(output, cv2.COLOR_GRAY2BGR)
+        dst = cv2.cornerHarris(gray,17,9,0.04)
+        ret, dst = cv2.threshold(dst,0.1*dst.max(),255,0)
+        dst = np.uint8(dst)
+        ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+        corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+        print('--------------')
+        for i in range(1, len(corners)):
+            print(corners[i])
+        img[dst>0.1*dst.max()]=[0,0,255]
+        cv2.imshow('image', img)
         
-        # approximate the contour
-        for c in contours:
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, eps * peri, True)
-            #approx = [x for x in approx if len(x) >= 3 & len(x) <= 6]
-            # print(approx)
-            # print(approx.num_pts)
-            # print([len(x[0]) for x in approx])
-            if len(approx) == 4:
-                cv2.drawContours(output, [approx], -1, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), 3)
-        text = "eps={:.4f}, num_pts={}".format(eps, len(approx))
-        # cv2.putText(output, text, (0, 0 - 15), cv2.FONT_HERSHEY_SIMPLEX,
-        #     0.9, (0, 255, 0), 2)
-        # show the approximated contour image
-        print("[INFO] {}".format(text))
-        cv2.imshow("Approximated Contour", output)
         if cv2.waitKey(0) & 0xFF == ord('q'):
             break
     
