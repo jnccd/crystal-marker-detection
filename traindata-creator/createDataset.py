881289bc581bd5300e0b396097b14ed857f08533
Niklas Carstensen <kobidogao@yahoo.com>
2023-11-13 19:31:12 +0100

    Remove borders in segpet cutouts

--- traindata-creator/createDataset.py
+++ traindata-creator/createDataset.py
@@ -397,26 +397,37 @@ def build_segpet_dataset(in_imgs, target_polys):
     for group in data_groups:
         for i, (in_img, polys) in enumerate(zip(in_imgs[group], target_polys[group])):
             for j, poly in enumerate(polys):
+                # Rotate
+                in_img_size_wh = tuple(reversed(in_img.shape[:2]))
+                rotation_angle = (random.random() - 0.5) * 2 * 270
+                rot_in_img, [poly] = homogeneous_mat_transform(in_img, [poly], in_img_size_wh, 
+                    cv2.getRotationMatrix2D(
+                        (poly.centroid.x, poly.centroid.y), 
+                        rotation_angle, 
+                        1), 
+                    background_color=background_color, 
+                    border_type=border_type, 
+                    min_label_visiblity=0.01)
+                
                 # Prepare bbox
                 poly: Polygon = poly # For linting
                 b = [int(x) for x in inflate_bbox_xyxy(poly.bounds, 0.3)]
+                bw = b[3] - b[1]
+                bh = b[2] - b[0]
+                bdiff = abs(bw - bh)
+                if bw > bh:
+                    b[0] -= int(bdiff/2)
+                    b[2] += int(bdiff/2)
+                else:
+                    b[1] -= int(bdiff/2)
+                    b[3] += int(bdiff/2)
                 
                 # Prepare img cutout
-                crop_img = in_img[b[1]:b[3], b[0]:b[2]]
+                crop_img = rot_in_img[b[1]:b[3], b[0]:b[2]]
                 if crop_img.shape[0] <= 0 or crop_img.shape[1] <= 0:
                     continue
-                crop_img_size_wh = tuple(reversed(crop_img.shape[:2]))
                 poly: Polygon = transform(poly, lambda x: np.array([(p[0] - b[0], p[1] - b[1]) for p in x] ))
-                # Rotate
-                rotation_angle = (random.random() - 0.5) * 2 * 270
-                crop_img, [poly] = homogeneous_mat_transform(crop_img, [poly], crop_img_size_wh, 
-                    cv2.getRotationMatrix2D(
-                        (crop_img.shape[1]/2, crop_img.shape[0]/2), 
-                        rotation_angle, 
-                        0.9), 
-                    background_color=background_color, 
-                    border_type=border_type, 
-                    min_label_visiblity=0.1)
+                
                 # Resize and pad
                 crop_img, [poly] = resize_and_pad_with_labels(crop_img, pet_target_size, [poly], background_color, border_type)
                 
